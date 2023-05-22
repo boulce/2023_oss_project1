a12ff38e9792fb1c83dc248d91b29ab2723343c4
Sun-JiHoon <cangogi1@naver.com>
2023-05-22 11:53:17 +0900

    git graph merge부분 나타내는 기능 구현

--- file-manager/src/main/java/com/github/filemanager/GitCommitHistory.java
+++ file-manager/src/main/java/com/github/filemanager/GitCommitHistory.java
@@ -25,13 +25,11 @@ class branchAndCommit{
     }
 }
 
-class textgraphAndBranchAndCommit{
+class textgraphAndCommit{
     public String textgraph;
-    public String branchname;
     public RevCommit commitname;
-    public textgraphAndBranchAndCommit(String text, String name, RevCommit tempCommit){
+    public textgraphAndCommit(String text, RevCommit tempCommit){
         textgraph = text;
-        branchname =name;
         commitname = tempCommit;
     }
 }
@@ -52,6 +50,7 @@ public class GitCommitHistory {
             List<Ref> branches = git.branchList().call();
 
             // 현재 브랜치와 이름이 일치하는 커밋 가져오기
+            int numOfCommits=0;
             List<branchAndCommit> commits = new ArrayList<>();
             for (Ref branch : branches) {
                 Iterable<RevCommit> branchCommits = git.log().add(branch.getObjectId()).call();
@@ -59,10 +58,12 @@ public class GitCommitHistory {
                 if(branchName.equals(branch.getName().substring(branch.getName().lastIndexOf('/') + 1))){
                     System.out.println(branch.getName());
                     for (RevCommit commit : branchCommits) {
+                        numOfCommits++;
                         commits.add(new branchAndCommit(branch.getName(), commit));
                     }
                 }
             }
+
             // git log 출력
             for (branchAndCommit commit : commits) {
                 System.out.println(
@@ -73,16 +74,42 @@ public class GitCommitHistory {
                 //"Author: " + commit.getAuthorIdent().getName() + " <" + commit.getAuthorIdent().getEmailAddress() + ">"
             }
 
+
+
             //dfs방식 구현
+            ArrayList<textgraphAndCommit> forPrint = new ArrayList<>();
+
+            int lineCount =0;
             Stack<branchAndCommit> commitStack = new Stack<>();
+            ArrayList<String> visitedCheckSum = new ArrayList<>();
+
             commitStack.push(commits.get(0));
             while(!commitStack.empty()){
                 branchAndCommit bac = commitStack.pop();
-                System.out.println(bac.commitname.getShortMessage());
-                for(RevCommit node: bac.commitname.getParents()){//부모 노드 호출
-                    //스택에 넣기
-                    commitStack.push(new branchAndCommit(branchName, node));
+                //System.out.println(bac.commitname.getShortMessage());
+                addCommitForPrint(forPrint,bac.commitname,lineCount);
+                visitedCheckSum.add(bac.commitname.getId().getName());//checkSum 가져오기
+                //if((bac.commitname.getParents().length == 0)){ //갈 수 있는 부모 노드 수를 반환하는 함수.
+                if(getCangoCount(visitedCheckSum, bac.commitname.getParents()) == 0){ //갈 수 있는 부모 노드 수를 반환하는 함수.
+                    //부모노드가 0이거나 있더라도 이미 방문한 노드면
+                    //줄 수를 늘릴 것이다.
+                    lineCount++;
+                    //System.out.println(lineCount);
                 }
+                for(RevCommit node: bac.commitname.getParents()){//부모 노드 호출
+                    //visitedCheckSum에 같은게 있다면 이미 방문한 거고 아니면 방문한 적 없는 것.
+                    if(visitedCheckSum.contains(node.getId().getName())){
+                        //이미 방문함.
+                    }
+                    else{
+                        //스택에 넣기
+                        commitStack.push(new branchAndCommit(branchName, node));
+                    }
+                }
+            }
+
+            for(textgraphAndCommit forprint_ : forPrint){
+                System.out.println(forprint_.textgraph + forprint_.commitname.getShortMessage());
             }
 
             /*
@@ -159,7 +186,49 @@ public class GitCommitHistory {
         return graphBuilder.toString();
     }
 
+    private static int getCangoCount(ArrayList<String> visitedCS, RevCommit[] nodes){
+        int result = nodes.length;
+        for(RevCommit node : nodes){
+            if(visitedCS.contains(node.getId().getName())){
+                result--;
+            }
+        }
+        return result;
+    }
+    private static void addCommitForPrint(ArrayList<textgraphAndCommit> forPrintList,RevCommit node,int lineNum) {
+        //forPrintList.add(new textgraphAndCommit("123",node));
 
+        int insertindex = calIndex(forPrintList, node, lineNum);
+        String tempStr = "";
+        for(int i=0;i<lineNum;i++){
+            tempStr += "|____";
+        }
+        tempStr += "*____";
+        forPrintList.add(insertindex, new textgraphAndCommit(tempStr ,node));//해당 줄
+/*
+        int count=0;
+        for(textgraphAndCommit forPrint : forPrintList){
+            if(count < insertindex){//insertindex 이전
+                forPrint.textgraph += "_____";
+            }
+            else if(count == insertindex){//insertindex 해당줄
+            }
+            else{//insertindex 이후
+                forPrint.textgraph += "|____";
+            }
+            count++;
+        }*/
+    }
 
-    //////////
+    private static int calIndex(ArrayList<textgraphAndCommit> forPrintList,RevCommit node,int lineNum){
+
+        int num=0;
+        for(textgraphAndCommit forPrint : forPrintList){
+            if(forPrint.commitname.getCommitTime() < node.getCommitTime()){ //node가 더 나중에 커밋된 경우.
+                break;
+            }
+            num++;
+        }
+        return num;
+    }
 }
