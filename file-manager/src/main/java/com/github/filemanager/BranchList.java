7a48bf316d39e2814c2dd50257398912b9c9c00c
Habin Song <boulce.email@gmail.com>
2023-05-23 11:37:12 +0900

    테이블에 브랜치 이름 목록 뜨게 함

--- file-manager/src/main/java/com/github/filemanager/BranchList.java
+++ file-manager/src/main/java/com/github/filemanager/BranchList.java
@@ -27,39 +27,22 @@ public class BranchList extends JFrame {
             public Class<?> getColumnClass(int column) {
                 return Object.class;
             }
+
+            @Override
+            public boolean isCellEditable(int row, int column) { // 테이블 내용 수정 못하게 한다.
+                return false;
+            }
         });
+        DefaultTableModel model = (DefaultTableModel) branch_list_table.getModel();
 
-//        table_commit.setRowHeight(10 + rowIconPadding);//commit 테이블의 행 높이 조정
+        List<Ref> call = git.branchList().call();
+        for (Ref branch_info : call) {
+                System.out.println("Branch: " + branch_info + " " + branch_info.getName() + " " + branch_info.getObjectId().getName());
+                Object[] rowData = {branch_info.getName()}; // 첫 번재 값은 아이콘(영헌이가 추가해야함), 두 번째는 파일 이름, // 세 번째는 파일의 절대 경로 (최상단 부모 깃 절대경로 + 파일의 상대경로)
+                model.addRow(rowData);
+        }
 
-       // DefaultTableModel model = (DefaultTableModel) branch_list_table.getModel();
-
-
-//        for (String stagedFile : stagedSet) {
-//            File stagedFILE = new File(stagedFile);
-//            if (status.getAdded().contains(stagedFile) || status.getChanged().contains(stagedFile)) { //added, changed는 모두 파일이 추가되거나, 변경된 상태에서 staged된것.
-//
-//                ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("staged.png"));
-//                Image image = icon.getImage();
-//                Image newimg = image.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-//
-//
-//                Object[] rowData = {new ImageIcon(newimg), stagedFILE.getName(), OsUtils.getAbsolutePathByOs(temp + "\\" + stagedFILE.getName())}; // 첫 번재 값은 아이콘(영헌이가 추가해야함), 두 번째는 파일 이름, // 세 번째는 파일의 절대 경로 (최상단 부모 깃 절대경로 + 파일의 상대경로)
-//                model.addRow(rowData);
-//
-//
-//            } else { //removed =deleted
-//
-//                ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("removed.png"));
-//                Image image = icon.getImage();
-//                Image newimg1 = image.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-//
-//                Object[] rowData = {new ImageIcon(newimg1), stagedFILE.getName(), OsUtils.getAbsolutePathByOs(temp + "\\" + stagedFILE.getName())}; // 첫 번재 값은 아이콘(영헌이가 추가해야함), 두 번째는 파일 이름, // 세 번째는 파일의 절대 경로 (최상단 부모 깃 절대경로 + 파일의 상대경로)
-//                model.addRow(rowData);
-//            }
-//
-//
-//        }
-
+        //branch_list_table.setEnabled(false);
         JScrollPane scrollpane = new JScrollPane(branch_list_table); // branch_list_table의 JScrollPane 생성
         //this.add(scrollpane , BorderLayout.CENTER);
         panel.add(scrollpane);
@@ -68,9 +51,5 @@ public class BranchList extends JFrame {
         this.add(panel);
 
         System.out.println("Listing local branches:");
-        List<Ref> call = git.branchList().call();
-        for (Ref ref : call) {
-            System.out.println("Branch: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
-        }
     }
 }
