2636cb4eceb1848287494beb288770c9209483f0
Habin Song <boulce.email@gmail.com>
2023-05-23 23:36:32 +0900

    merge 할 때 Already up-to-date 예외처리

--- file-manager/src/main/java/com/github/filemanager/BranchMerge.java
+++ file-manager/src/main/java/com/github/filemanager/BranchMerge.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 import java.io.IOException;
 import java.util.Iterator;
 
+import static org.eclipse.jgit.api.MergeResult.MergeStatus.ALREADY_UP_TO_DATE;
+
 
 public class BranchMerge {
     /*
@@ -30,19 +32,26 @@ public class BranchMerge {
 
 
             // merge가 성공적으로 되었는지 안되었는지 판단해서 창 띄우는 부분, 아직 제대로 작동 안함. 구현중
-            if (mergeResult.getMergeStatus().isSuccessful() ) {
-                JOptionPane.showMessageDialog(null, "Merging \"" + target_branch_name + "\" to \"" + current_branch_name + "\" was done successfully!");
-                return true;
-            } else {
-                if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.CONFLICTING)){
+            if (mergeResult.getMergeStatus().isSuccessful() ) { // Merge가 Successful
+                if(mergeResult.getMergeStatus() == ALREADY_UP_TO_DATE){ // 합치려는 브랜치의 내용을 현재 브랜치가 알고 있는 경우 Already up-to-date
+                    JOptionPane.showMessageDialog(null, "[Error] Already up-to-date");
+                    return false;
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Merging \"" + target_branch_name + "\" to \"" + current_branch_name + "\" was done successfully!");
+                    System.out.println(mergeResult.getMergeStatus());
+                    return true;
+                }
+            } else { // Merge가 Not Successful
+                if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.CONFLICTING)){ // Conflict 난 경우
                     String unmerged_paths = "";
                     Iterator<String> keys = mergeResult.getConflicts().keySet().iterator();
-                    while (keys.hasNext()){
+                    while (keys.hasNext()){ // unmerged_paths 를 가져옴
                         String key = keys.next();
                         unmerged_paths += key + ", ";
                     }
                     unmerged_paths = unmerged_paths.substring(0, unmerged_paths.length()-2); // unmerged_paths의 마지막 ', '를 제거한다
-                    JOptionPane.showMessageDialog(null, "Conflict happened!" + "\nUnmerged Paths: " + unmerged_paths);
+                    JOptionPane.showMessageDialog(null, "[Error] Conflict happened!" + "\nUnmerged Paths: " + unmerged_paths);
 
                     /*
                         git merge --abort
@@ -55,14 +64,14 @@ public class BranchMerge {
                     // reset the index and work directory to HEAD
                     Git.wrap(git.getRepository()).reset().setMode(ResetCommand.ResetType.HARD).call();
                 }
-                else{
-                    JOptionPane.showMessageDialog(null, mergeResult.getMergeStatus());
+                else{ // conflict 외에 successful 하지 않을 경우에 예외 처리
+                    JOptionPane.showMessageDialog(null, "[Error] " + mergeResult.getMergeStatus());
                 }
                 return false;
             }
         }
         else{ // 만약 target_branch와 current_branch가 같다면 merge 수행 안되도록 한다.
-            JOptionPane.showMessageDialog(null, "You can't choose the current branch");
+            JOptionPane.showMessageDialog(null, "[Error] You can't choose the current branch");
             return false;
         }
     }
