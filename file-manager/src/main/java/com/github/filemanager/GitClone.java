5499f5f4d552590abaee3cb5539a773902525127
DESKTOP-65EJS6N\yhyh5 <yhyh5275@naver.com>
2023-05-27 19:56:27 +0900

    gwak commit 6.1 .txt정책 관련 이슈, 이미 있는 ID의 경우 처리분기 필요

--- file-manager/src/main/java/com/github/filemanager/GitClone.java
+++ file-manager/src/main/java/com/github/filemanager/GitClone.java
@@ -5,6 +5,7 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
+import javax.swing.*;
 import java.io.*;
 import java.util.HashMap;
 import java.io.BufferedReader;
@@ -44,10 +45,17 @@ public class GitClone {
                 String existkey=parts[0];
                 String existvalue = parts[1];
 
-                if(existkey.equals(gitID)) {
-                    sb.append(gitID).append(" ").append(accessToken).append(System.lineSeparator());
-                    idExists=true;
-                } else {
+                if(existkey.equals(gitID)) { //새로운 ID가 setting에 존재하는 경우
+
+                    int choice=JOptionPane.showConfirmDialog(null,"해당 ID가 존재합니다. 이전 token을 사용하시겠습니까?"," ", JOptionPane.YES_NO_OPTION);
+
+                    if(choice==JOptionPane.NO_OPTION) { //새로운 토큰을 사용
+                        sb.append(gitID).append(" ").append(accessToken).append(System.lineSeparator());
+                        idExists = true;
+                    } else { //setting.txt에 있는 토큰 사용
+
+                    }
+                } else { //새로운 ID가 setting에 존재하지 않는 경우
                     sb.append(line).append(System.lineSeparator());
                 }
 
@@ -57,7 +65,7 @@ public class GitClone {
         }
         br.close();
 
-        if(!idExists) {
+        if(!idExists) { //새로운 ID가 setting.txt에 존재하지 않으면 setting에 추가.
             sb.append(gitID).append(" ").append(accessToken).append(System.lineSeparator());
         }
 
@@ -68,7 +76,7 @@ public class GitClone {
     }
 
     public void cloneRepoPrivate(String repoUrl, String gitID, String accessToken,String localPath) throws GitAPIException, IOException {
-        String settingpath=System.getProperty("user.dir")+"\\src\\main\\resources\\settings.txt";
+        String settingpath=OsUtils.getAbsolutePathByOs(System.getProperty("user.dir")+"\\src\\main\\resources\\settings.txt");
 
 
         try {
@@ -81,13 +89,13 @@ public class GitClone {
         } catch (GitAPIException e) {
 
             ReadWriteSetting(gitID,accessToken,settingpath);
-            //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
+            //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
 
 
             Git.cloneRepository()
                     .setURI(repoUrl)
                     .setDirectory(new File(localPath))
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(gitID, accessToken)) //email도 되고, username도 가능
+                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(gitID, accessToken))
                     .call();
 
 
