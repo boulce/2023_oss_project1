87d1395517735f2e164a918961697069ef244701
DOL-DOM <haesolj@naver.com>
2023-05-30 20:28:17 +0900

    불필요한 Branchname 변수 삭제

--- file-manager/src/main/java/com/github/filemanager/BranchManagement.java
+++ file-manager/src/main/java/com/github/filemanager/BranchManagement.java
@@ -12,7 +12,7 @@ import org.eclipse.jgit.lib.Ref;
 
 
 public class BranchManagement extends JFrame {
-    private JTextField branchName;
+
 
     //1. 현재 브랜치 이름 띄우기 -현재 경로가 깃으로 관리 되고 있는지 확인, 관리되고 있으면 파일매니저 우측 하단 패널에 브랜치명 띄우기
     public String BranchName(Git git, File currentPath, Repository repository) {
