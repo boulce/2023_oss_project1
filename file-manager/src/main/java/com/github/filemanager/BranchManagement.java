68a8149077b0617a8b26ab9ac8a27b605e8ac577
DOL-DOM <haesolj@naver.com>
2023-05-30 15:47:26 +0900

    f1_branchDelete 함수 구현

--- file-manager/src/main/java/com/github/filemanager/BranchManagement.java
+++ file-manager/src/main/java/com/github/filemanager/BranchManagement.java
@@ -1,13 +1,15 @@
 package com.github.filemanager;
+
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.Status;
-import com.github.filemanager.GitUtilsForTrack.*;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.Ref;
+
 
 
 public class BranchManagement  extends JFrame{
@@ -15,22 +17,7 @@ public class BranchManagement  extends JFrame{
 
     //1. 현재 브랜치 이름 띄우기 -현재 경로가 깃으로 관리 되고 있는지 확인, 관리되고 있으면 파일매니저 우측 하단 패널에 브랜치명 띄우기
     public void BranchName (Git git, File currentPath){
-        if (GitUtilsForTrack.isGitRepository(currentPath)) {
-            Status status = null;
-            try {
-                status = git.status().call();
 
-            } catch (GitAPIException e) {
-                throw new RuntimeException(e);
-            }
-
-        } else {
-            try {
-
-            } catch (Exception e) {
-
-            }
-        }
     }
 
 
@@ -40,15 +27,32 @@ public class BranchManagement  extends JFrame{
 
     }
 
+    public String[] getBranchNamesList(Git git, String current_branch) throws GitAPIException {
+        String[] branchNames = new String[git.branchList().call().size() - 1];
+        int i = 0;
 
+        for (Ref ref : git.branchList().call()) {
+            // refs/head/actual_name
+            String name = ref.getName().split("/")[2];
+            if (!current_branch.equals(name))
+                branchNames[i++] = ref.getName().split("/")[2];
+        }
 
-    //3. Error Popup
+        return branchNames;
+    }
 
     //3. Branch Delete
+    public void BranchDelete(Git git, String name) throws GitAPIException {
+       //예외처리 추가하기 - 삭제하려는 브랜치가 머지되지 않은 경우, 삭제할 수 없다는 오류 팝업
+            git.branchDelete().setBranchNames(name).call();
+
+        }
+    }
+
 
     //4. Branch Rename
 
     //5. Branch Checkout
 
 
-}
+
